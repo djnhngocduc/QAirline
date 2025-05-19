cdfeb5d3596a3d83da1027d4f1cc04cc14f90512
Đinh Ngọc Đức <22028166@vnu.edu.vn>
2025-05-20 01:48:09 +0700

    create sidebar for profile

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,8 +1,14 @@
+import HeaderProfile from "./HeaderProfile";
+import SidebarProfile from "./SidebarProfile";
+
 function Profile() {
   return (
-    <>
-      Profile
-    </>
+    <div className="h-full min-h-px overflow-x-hidden">
+      <HeaderProfile />
+      <div className="md: flex flex-col justify-center overflow-hidden pt-9 md:flex-row">
+        <SidebarProfile />
+      </div>
+    </div>
   )
 }
 
---
+++ frontend/src/pages/Profile/SidebarProfile.jsx
@@ -0,0 +1,76 @@
+import { useState, useEffect } from 'react';
+import { Card, CardContent, CardHeader, CardTitle } from '../../components/ui/Card';
+import { Avatar, AvatarImage, AvatarFallback } from '../../components/ui/Avatar';
+
+const SidebarProfile = () => {
+  const [userData, setUserData] = useState({
+    profilePictureUrl: '',
+    fullName: '',
+    membershipNumber: '',
+    avios: 0,
+    qpoints: 0,
+    qcredits: 0,
+  });
+
+  const [shortName, setShortName] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          'http://localhost:5000/api/customer/my-info',
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem('token')}`,
+            },
+          }
+        );
+        const data = await response.json();
+        setUserData({
+          profilePictureUrl: data.User.profilePicture,
+          fullName: `${data.title} ${data.first_name} ${data.middle_name} ${data.last_name}`,
+          membershipNumber: data.id * data.user_id * 653432,
+          avios: data.avios || 0,
+          qpoints: data.qpoints || 0,
+          qcredits: data.qcredits || 0,
+        });
+        setShortName(data.first_name.charAt(0) + data.last_name.charAt(0));
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <aside className="w-full px-3 md:w-64 md:max-w-sm md:px-0">
+      <Card className="rounded-md border-none bg-gradient-to-r from-secondary-foreground to-[#ad5389] py-5 text-center text-white">
+        <CardHeader>
+          <Avatar className="mx-auto h-24 w-24">
+            <AvatarImage
+              src={userData.profilePictureUrl}
+              alt={userData.fullName}
+            />
+            <AvatarFallback className="text-3xl text-gray-700">
+              {shortName}
+            </AvatarFallback>
+          </Avatar>
+          <CardTitle className="mt-4">{userData.fullName}</CardTitle>
+        </CardHeader>
+        <CardContent className="p-0">
+          <p className="text-sm">
+            Membership number: <strong>{userData.membershipNumber}</strong>
+          </p>
+          <div className="mt-4 space-y-1 text-sm">
+            <p>Apoints: 5</p>
+            <p>Qpoints: 10</p>
+            <p>_________________________</p>
+          </div>
+        </CardContent>
+      </Card>
+    </aside>
+  );
+};
+
+export default SidebarProfile;
