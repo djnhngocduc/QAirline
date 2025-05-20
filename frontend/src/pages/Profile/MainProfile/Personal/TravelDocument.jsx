c2b98d91f1fb3cdc8df6bb5ede89e42952f640d4
Đinh Ngọc Đức <22028166@vnu.edu.vn>
2025-05-20 12:45:37 +0700

    create travel document for profile

---
+++ frontend/src/pages/Profile/MainProfile/Personal/TravelDocument.jsx
@@ -0,0 +1,47 @@
+const TravelDocument = () => {
+  return (
+    <div className="md:border-b md:border-gray-300 md:pb-6">
+      <div className="mb-4 flex items-center justify-between">
+        <h2 className="text-lg font-semibold text-gray-700">
+          Your travel documents
+        </h2>
+        {/* <button className="flex items-center gap-1 text-secondary">
+          <Pencil className="h-4 w-4 text-secondary" />
+          Edit
+        </button> */}
+      </div>
+      <div className="grid grid-cols-2 gap-4">
+        <div>
+          <p className="font-semibold text-gray-800">Passport Details</p>
+          <p className="text-gray-600">N/A</p>
+        </div>
+        <div>
+          <p className="font-semibold text-gray-800">Nationality</p>
+          <p className="text-gray-600">N/A</p>
+        </div>
+        <div>
+          <p className="font-semibold text-gray-800">Passport number</p>
+          <p className="text-gray-600">N/A</p>
+        </div>
+        <div>
+          <p className="font-semibold text-gray-800">Expiry date</p>
+          <p className="text-gray-600">N/A</p>
+        </div>
+        <div>
+          <p className="font-semibold text-gray-800">National ID number</p>
+          <p className="text-gray-600">N/A</p>
+        </div>
+        <div>
+          <p className="font-semibold text-gray-800">Redress number</p>
+          <p className="text-gray-600">N/A</p>
+        </div>
+        <div>
+          <p className="font-semibold text-gray-800">Known traveler number</p>
+          <p className="text-gray-600">N/A</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TravelDocument;
